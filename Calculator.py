bbf389d57fcd403aa802b6ad51131253e33688b1
mushfiqulhoque1001-tech <mushfiqulhoque1001@gmail.com>
2026-06-01 19:04:06 +0600

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+a=(input("Enter first number:" ))
+b=(input("Enter second number:" ))
+print("Sum:{a+b}") 
+print("Difference:{a-b}")
+print("Product:{a*b}")
+print("Division:{a/b if b != 0 else 'undefined'}")
+print("Modulus:{a%b}")
+print("Exponentiation:{a**b}")
+
